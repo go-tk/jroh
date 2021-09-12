b40f7b43100d6b516978f94b66afb63e34ba7395
Roy O'Young <roy2220@outlook.com>
2021-09-12 20:25:56 +0800

    Bump version: 0.0.0 → 0.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name="jroh",
-    version="0.0.0",
+    version="0.1.0",
     description="JSON-RPC over HTTP",
     packages=["jroh"],
     package_dir={"jroh": "src/jroh"},
