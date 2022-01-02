854ea505898dbd873436405f27875028f7862693
Roy O'Young <roy2220@outlook.com>
2022-01-02 17:25:31 +0800

    Bump version: 0.7.1 → 0.8.0

--- examples/output/go/go.mod
+++ examples/output/go/go.mod
@@ -2,6 +2,6 @@ module github.com/go-tk/jroh/examples/output/go
 
 go 1.16
 
-require github.com/go-tk/jroh/go v0.7.1
+require github.com/go-tk/jroh/go v0.8.0
 
 replace github.com/go-tk/jroh/go => ../../../go
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name="jroh",
-    version="0.7.1",
+    version="0.8.0",
     description="JSON-RPC over HTTP",
     packages=["jroh"],
     package_dir={"jroh": "src/jroh"},
