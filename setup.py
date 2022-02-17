24511ebf8d943b6e310604369cbd8b3f0eb20eaa
Roy O'Young <roy2220@outlook.com>
2022-02-18 00:15:17 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,15 +6,15 @@ setup(
     description="JSON RPC over HTTP",
     packages=["jroh"],
     package_dir={"jroh": "src/jroh"},
+    entry_points={
+        "console_scripts": [
+            "jrohc=jroh.compiler:main",
+        ],
+    },
     python_requires=">=3.9",
     install_requires=[
         "PyYAML==5.4.1",
         "Mako==1.1.4",
         "google-re2==0.2.20210901",
     ],
-    entry_points={
-        "console_scripts": [
-            "jrohc=jroh.compiler:main",
-        ],
-    },
 )
